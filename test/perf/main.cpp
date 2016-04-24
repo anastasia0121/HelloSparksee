d8cdad46180fc127d08a5fb2e8b78693a006abca
Anastasia Kondratieva <anastasia012@yandex.ru>
2016-04-24 09:51:56 +0300

    fix test

--- test/perf/main.cpp
+++ test/perf/main.cpp
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 bool unit_test();
-void perf_test();
+void perf_test(const int it);
 
 int main(int argc, char *argv[]) 
 {
 //    if (!unit_test()) return -1;   
-    perf_test();
+    perf_test(10000);
 
     return 0;
 }
--- test/perf/perf.cpp
+++ test/perf/perf.cpp
@@ -23,9 +23,9 @@ uint64_t init_rate()
     return rate;
 }
 
-uint64_t to_usec(uint64_t tsc, uint64_t rate)
+double to_msec(uint64_t tsc, uint64_t rate)
 {
-    return tsc / (rate / 1000000);
+    return static_cast<double>(tsc) / (rate / 1000);
 }
 
 template <typename T, size_t N>
@@ -33,8 +33,8 @@ void print_statistics(std::array<T,N> & percentile_map, std::array<T,N> & percen
 {
     int count = N;
 
-    std::sort(percentile_map.begin(), percentile_map.end(), std::less<uint64_t>());
-    std::sort(percentile_switch.begin(), percentile_switch.end(), std::less<uint64_t>());
+    std::sort(percentile_map.begin(), percentile_map.end(), std::less<T>());
+    std::sort(percentile_switch.begin(), percentile_switch.end(), std::less<T>());
 
     std::cout << std::setw(16) << "|" 
               << std::setw(20) << " 50% |"
@@ -44,25 +44,25 @@ void print_statistics(std::array<T,N> & percentile_map, std::array<T,N> & percen
               << std::setw(20) << " 99% |" 
               << std::endl
               << "switch-case:   |" 
-              << std::setw(15) << percentile_switch[50*100/count - 1]  <<  " us |" 
-              << std::setw(16) << percentile_switch[70*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_switch[90*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_switch[95*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_switch[100*100/count - 1]  <<  " us |" 
+              << std::setw(15) << percentile_switch[50*100/count - 1]  <<  " ms |" 
+              << std::setw(16) << percentile_switch[70*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_switch[90*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_switch[95*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_switch[100*100/count - 1]  <<  " ms |" 
               << std::endl
               << "unordered_map: |" 
-              << std::setw(15) << percentile_map[50*100/count - 1]  <<  " us |" 
-              << std::setw(16) << percentile_map[70*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_map[90*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_map[95*100/count - 1]  <<  " us |" 
-              << std::setw(15) << percentile_map[100*100/count - 1]  <<  " us |" << std::endl;
+              << std::setw(15) << percentile_map[50*100/count - 1]  <<  " ms |" 
+              << std::setw(16) << percentile_map[70*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_map[90*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_map[95*100/count - 1]  <<  " ms |" 
+              << std::setw(15) << percentile_map[100*100/count - 1]  <<  " ms |" << std::endl;
 }
 
-void perf_test()
+void perf_test(const int it)
 {
     const int count = 100;
-    std::array<uint64_t, count> percentile_map;
-    std::array<uint64_t, count> percentile_switch;
+    std::array<double, count> percentile_map;
+    std::array<double, count> percentile_switch;
 
     std::wstring src, dst;
     src = L"qertyuiop[]asdfghjkl;'zxcvbnm,.1234567890йцукенгшщзхъфывапролджэячсмитьбю.";
@@ -72,18 +72,19 @@ void perf_test()
     for (int i = 0; i < count; ++i)
     {
         volatile uint64_t t3 = rdtsc();
-        for (int i = 0; i < 1000; ++i)
+        for (int j = 0; j < it; ++j)
             switch_keyboard_layout1(src, dst);
         volatile uint64_t t4 = rdtsc();
-        percentile_map[i] = t4 - t3;
+
+        percentile_map[i] = to_msec(t4 - t3, rate);
 
         volatile uint64_t t1 = rdtsc();
-        for (int i = 0; i < 1000; ++i)
+        for (int j = 0; j < it; ++j)
             switch_keyboard_layout(src, dst);
         volatile uint64_t t2 = rdtsc();
-        percentile_switch[i] = t2 - t1;
-    }
 
+        percentile_switch[i] = to_msec(t2 - t1, rate);
+    }
 
     print_statistics(percentile_map, percentile_switch);
 }
